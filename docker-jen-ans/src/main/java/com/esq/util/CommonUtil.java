c7272475506baa58ce6a48a0e37918c43c0324eb
heshuiqing <1826818052@qq.com>
2019-06-28 18:29:16 +0800

    docker-jen-ans

---
+++ docker-jen-ans/src/main/java/com/esq/util/CommonUtil.java
@@ -0,0 +1,33 @@
+package com.esq.util;
+
+/**
+* @ClassName: 系统常量
+* @Description: 各种常量
+* @author esqabc
+* @date 2019-09-09
+ */
+public class CommonUtil {
+
+
+	/**（www.esqabc.com)*/
+	public static final String ESQABC_WEBSITE= "www.esqabc.com";
+	/**（www.esqabc.com)*/
+	public static final String ESQABC_= "www.esqabc.com";
+	
+	/**（www.esqabc.com)*/
+	public static final String ESQABC_A= "www.esqabc.com";
+	
+	
+	/**（www.esqabc.com)*/
+	public static final String ESQABC_B= "www.esqabc.com";
+	
+	
+	
+	/**（www.esqabc.com)*/
+	public static final String ESQABC_C= "www.esqabc.com";
+	
+	
+	
+	/**（www.esqabc.com)*/
+	public static final String ESQABC_D= "www.esqabc.com";
+}
